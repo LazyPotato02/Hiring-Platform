a0c4b6436470e5cb631b0142d6a64a2d1f4c0633
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-05-18 17:43:28 +0300

    Added logout functionality

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from users.views import RegisterView
+from users.views import RegisterView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,8 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import CustomUser
@@ -24,3 +26,20 @@ class RegisterView(CreateAPIView):
         }
         return Response(response_data)
 
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            if not refresh_token:
+                return Response({"error": "Refresh token is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
