83d2bd5ce2110e3aa0a76a4a9b341625aa537453
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-05-22 21:36:26 +0300

    Added Jobs Category and tech stack, saparate model for them is created

--- backend/job/models.py
+++ backend/job/models.py
@@ -3,10 +3,25 @@ from django.db import models
 from company.models import Company
 
 
+class TechCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TechStack(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey(TechCategory, on_delete=models.CASCADE, related_name="tech_stacks")
+
+    def __str__(self):
+        return self.name
+
+
 class Job(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    tech_stack = models.CharField(max_length=255)
+    tech_stack = models.ManyToManyField(TechStack, related_name="jobs")
     is_active = models.BooleanField(default=True)
     posted_at = models.DateTimeField(auto_now_add=True)
